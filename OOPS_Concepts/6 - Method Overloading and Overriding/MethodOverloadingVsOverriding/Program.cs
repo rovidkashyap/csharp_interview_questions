d6e33b2bba46a8455637214760757924eb31b8c9
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-18 17:44:50 +0530

    Question 6

---
+++ OOPS_Concepts/6 - Method Overloading and Overriding/MethodOverloadingVsOverriding/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace MethodOverloadingVsOverriding
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // As We Already discussed with Example that what is Method Overloading and Overriding.
+
+            // METHOD OVERLOADING (a.k.a Compile-Time Polymorphism)
+            // Achieved by having multiple methods with the same name but different parameter lists.
+            // Resolved at compile-time.
+            // Example: Multiple 'Add' methods in the previous example of 'MathOperations' class.
+
+            // METHOD OVERRIDING ( a.k.a Runtime Polymorphism)
+            // Achieved by overriding a base class method in a derived class.
+            // Resolved at runtime.
+            // Example: 'MakeSound' method in the previous example of 'Animal', 'Dog' and 'Cat' classes.
+        }
+    }
+}
