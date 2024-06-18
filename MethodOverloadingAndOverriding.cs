5ffcddfb7330695adbda5f2f2d20e85fbf3bff9f
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-18 17:40:46 +0530

    Question 6

---
+++ MethodOverloadingAndOverriding.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+		// As We Already discussed with Example that what is Method Overloading and Overriding.
+
+		// METHOD OVERLOADING (a.k.a Compile-Time Polymorphism)
+		// Achieved by having multiple methods with the same name but different parameter lists.
+		// Resolved at compile-time.
+		// Example: Multiple 'Add' methods in the previous example of 'MathOperations' class.
+
+		// METHOD OVERRIDING ( a.k.a Runtime Polymorphism)
+		// Achieved by overriding a base class method in a derived class.
+		// Resolved at runtime.
+		// Example: 'MakeSound' method in the previous example of 'Animal', 'Dog' and 'Cat' classes.
+	}
+}
