77914c7802501164cf92501ae8bc85477fef8e3d
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-18 22:13:21 +0530

    Question 8

---
+++ OOPS_Concepts/8 - CallByValue VS CallByReference/CallByReference/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CallByReference
+{
+
+    // CALL BY REFERENCE
+    // A Reference of the value is passed to the method.
+    // Changes made to the parameter inside the method affect the original argument.
+    // Passing the reference to the argument, the original value is modified by the method.
+
+    public class CallByReference
+    {
+        public void ChangeValue(ref int number)
+        {
+            number = 100;           // This change affects the original argument
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int originalNumber = 50;
+            CallByReference example = new CallByReference();
+
+            Console.WriteLine("Before Call By Reference: " + originalNumber);       // Outputs: 50
+            example.ChangeValue(ref originalNumber);
+            Console.WriteLine("After Call By Reference: " + originalNumber);        // Outputs: 100
+        }
+    }
+}
---
+++ OOPS_Concepts/8 - CallByValue VS CallByReference/CallByValue/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CallByValue
+{
+
+    // CALL BY VALUE
+    // A copy of the value is passed to the method
+    // Changes made to the parameter inside the method do not affect the original argument.
+    // Passing a copy of the argument, the original value remains unchanged.
+
+    public class CallByValue
+    {
+        public void ChangeValue(int number)
+        {
+            number = 100;       // This change is local to the method
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int originalNumber = 50;
+            CallByValue example = new CallByValue();
+
+            Console.WriteLine("Before Call by Value: " + originalNumber);       // Outputs: 50
+            example.ChangeValue(originalNumber);
+            Console.WriteLine("After Call by Value: " + originalNumber);        // Outputs: 50
+        }
+    }
+}
