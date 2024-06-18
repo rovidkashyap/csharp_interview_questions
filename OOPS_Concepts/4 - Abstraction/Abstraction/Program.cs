7a6ff4c82c1f2d4b697d6206b67c8713e5910fb1
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-18 17:20:08 +0530

    Question 4

---
+++ OOPS_Concepts/4 - Abstraction/Abstraction/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Abstraction
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // As already defined in the second and third question about Abstract Classes and Abstract Methods
+            // Here we are only show you that how many ways through which we can achieved Abstraction
+
+            // Basically Abstraction is the concept of hiding the complex implementation details and exposing only the essential features of an object.
+            // Abstraction can be achieved using Abstract Classes
+            // Abstraction can be achieved using Interfaces
+
+            // NOW Questions Arises WHen to Use Abstract Classes and Interfaces
+            
+            // ABSTRACT CLASSES - When you have common base class that provides some default behavior and enforces certain methods to be implemented by derived classes.
+            // Abstract classes are useful when you want to share code among several closely related classes.
+            
+            // INTERFACES - When you need to define a contract that can be implemented by any class, regardless of its position in the class hierarchy. Interfaces are
+            // useful for providing a common interface across different classes and supporting multiple inheritance.
+
+            // By Using Abstraction, you can create more modular, maintainable and understandable code.
+        }
+    }
+}
