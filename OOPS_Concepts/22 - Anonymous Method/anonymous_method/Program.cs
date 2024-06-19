81dee4f64d91a364a8993fc45a807c7395e1e2d0
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-19 16:46:05 +0530

    Question 22

---
+++ OOPS_Concepts/22 - Anonymous Method/anonymous_method/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace anonymous_method
+{
+    // In C#, Anonymous Method are the method without name that is defined using the `delegate`
+    // keyword directly within the code.
+    // Anonymous methods provides a way to define inline methods at the point where they are needed
+    
+    public class Program
+    {
+        // Define a delegate
+        public delegate void DisplayMessage(string message);
+
+        static void Main(string[] args)
+        {
+            // Assign an anonymous method to the delegate
+            DisplayMessage messageDelegate = delegate (string message)
+            {
+                Console.WriteLine(message);
+            };
+
+            // Invoke the delegate
+            messageDelegate("Hello, World!");
+        }
+    }
+}
