4d82e7b2bd6bd07aaeb6fa012f37cb0b907e439d
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-19 15:29:32 +0530

    Question 19

---
+++ OOPS_Concepts/19 - Partial Class/Partial_Classes/MyClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Partial_Classes
+{
+    public partial class MyClass
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method 1");
+        }
+    }
+}
---
+++ OOPS_Concepts/19 - Partial Class/Partial_Classes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Partial_Classes
+{
+    // 1. Partial Class in C# provide felxibility in organizing and managing large class definitions.
+    // 2. Partial class allows a single class definition to be divided into multiple files.
+    // 3. Each part of class is marked with the `partial` keyword. When compiled, all the parts are
+    // combined into a single class definition.
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass obj = new MyClass();
+            obj.Method();
+            obj.Method1();
+        }
+    }
+
+    public partial class MyClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("Method");
+        }
+    }
+}
