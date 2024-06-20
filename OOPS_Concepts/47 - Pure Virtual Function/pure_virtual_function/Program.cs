38815a5bf8169cebfab958a34b3cfae12fdd4358
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-20 18:59:17 +0530

    Question 47

---
+++ OOPS_Concepts/47 - Pure Virtual Function/pure_virtual_function/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace pure_virtual_function
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // While C# does not have equivalent of C++ pure virtual functions,
+            // the combination of abstract classes and abstract methods provides
+            // similar functionlity. 
+
+            // This allows you to define methods that must be implemented by any
+            // derived class, enusring a consistent interface and promoting proper
+            // inheritance and polymorphism.
+        }
+    }
+}
