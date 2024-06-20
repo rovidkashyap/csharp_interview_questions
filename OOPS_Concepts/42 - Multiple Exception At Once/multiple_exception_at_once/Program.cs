b74e8ca623a9a9282ac458644dca975c92dea794
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-20 18:44:44 +0530

    Question 42

---
+++ OOPS_Concepts/42 - Multiple Exception At Once/multiple_exception_at_once/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace multiple_exception_at_once
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            #region USING MULTIPLE CATCH BLOCKS
+            try
+            {
+                ThrowMultipleExceptions();
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine($"Caught ArgumentNullException: {ex.Message}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"Caught InvalidOperationException: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Caught General Exception: {ex.Message}");
+            }
+            #endregion
+
+            #region SINGLE CATCH BLOCK WITH `WHEN`
+            try
+            {
+                ThrowMultipleExceptions();
+            }
+            catch (Exception ex) when (ex is ArgumentNullException || ex is InvalidOperationException)
+            {
+                Console.WriteLine($"Caught Specific Exception: {ex.GetType()} - {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Caught General Exception: {ex.Message}");
+            }
+            #endregion
+
+            #region USING `AGGREGATEEXCEPTION`
+            try
+            {
+                Task task1 = Task.Run(() => { throw new InvalidOperationException("Invalid operation"); });
+                Task task2 = Task.Run(() => { throw new ArgumentNullException("paramName", "Parameter cannot be null."); });
+
+                Task.WaitAll(task1, task2);
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var innerEx in ex.InnerExceptions)
+                {
+                    Console.WriteLine($"Caught Exception: {innerEx.GetType()} - {innerEx.Message}");
+                }
+            }
+            #endregion
+        }
+        static void ThrowMultipleExceptions()
+        {
+            // Simulate an exception
+            throw new ArgumentNullException("paramName", "Parameter cannot be null.");
+        }
+    }
+}
---
+++ OOPS_Concepts/42 - Multiple Exception At Once/multiple_exception_at_once/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("multiple_exception_at_once")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("multiple_exception_at_once")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("17e7cf75-f7c6-4f47-bd30-8932de65713a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
