29d5cd5f248e8ab0919fae06ddc3498a0911621e
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-20 14:28:09 +0530

    Question 35

---
+++ OOPS_Concepts/35 - Inherit Enum/inherit_enum/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace inherit_enum
+{
+    // No, In C#, enums (enumerations) do not support inheritance directly.
+    // Enums are value types that represent a set of named constants, and their values are typically integral types such as 'int'.
+    // Enums cannot inherit or inherited from other enums or classes.
+    // In C# We are using Classes instead of Inheriting Enums
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // No We Can't Inherit Enum in C# because Enums do not support inheritance.
+        }
+    }
+}
