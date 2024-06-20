d9a1999d07000b14f8cdbe87a938930f3a11df49
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-20 13:44:28 +0530

    Question 32

---
+++ OOPS_Concepts/32 - Sealed Method and Properties/sealed_method_and_properties/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace sealed_method_and_properties
+{
+    // SEALED METHOD is an overriden method that cannot be overriden again in any derived class.
+    // This is useful when you want to finalize the implementation of a method in derived class
+    // and prevent any further modification by subclass.
+
+    // SEALED PROPERTIES is a property that has been overriden and then sealed to prevent
+    // further overriding in any derived class.
+
+    #region SEALED CLASS AND PROPERTIES
+    public class BaseClass
+    {
+        // Virtual Method in base class
+        public virtual void Display()
+        {
+            Console.WriteLine("Display method in BaseClass");
+        }
+
+        // Virtual Property in base class
+        public virtual string Name { get; set; }
+    }
+    public class DerivedClass : BaseClass
+    {
+        // overriding the virtual method
+        public override void Display()
+        {
+            Console.WriteLine("Display method in DerivedClass");
+        }
+
+        // Overriding the virtual property
+        public override string Name { get; set; }
+    }
+    public class FurtherDerivedClass : DerivedClass
+    {
+        // Attempting to override a sealed method would cause a compile-time error
+        public sealed override void Display()
+        {
+            Console.WriteLine("Sealed Display methon in FurtherDerivedClass");
+        }
+
+        // Sealing the overriden property
+        public sealed override string Name { get; set; }
+    }
+    public class FinalClass : FurtherDerivedClass
+    {
+        // This would cause a compile-time error becuase the method is sealed in FurtherSerivedClass
+        //public override void Display()
+        //{
+        //    Console.WriteLine("Display method in FinalClass");
+        //}
+
+        // This would cause a compile-time error because the property is sealed in FurtherDerivedClass
+        //public override string Name { get; set; }
+    }
+    #endregion
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FinalClass obj = new FinalClass();
+            obj.Display();
+
+            obj.Name = "Rovid";
+            Console.WriteLine(obj.Name);
+        }
+    }
+}
---
+++ OOPS_Concepts/32 - Sealed Method and Properties/sealed_method_and_properties/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("sealed_method_and_properties")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("sealed_method_and_properties")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a0cbac96-a041-4212-8b63-d4c511279d3e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
