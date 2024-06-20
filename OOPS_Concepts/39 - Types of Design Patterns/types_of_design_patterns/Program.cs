071ccf1fff41f608fc3c340befcaa6e8d46e015c
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-20 16:25:35 +0530

    Question 39

---
+++ OOPS_Concepts/39 - Types of Design Patterns/types_of_design_patterns/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace types_of_design_patterns
+{
+    // There are three types of Design Patterns in C#:
+
+    // 1. Creational Patterns ->
+    //    1.1 - Singleton Patterns - This Pattern has only one instance and provides a global point of access to it.
+    //    1.2 - Factory Method - Defines an interface for creating objects, but let subclass decide which class to instantiate.
+    //    1.3 - Abstract Factory - Provide Interface for creating families of related or dependent objects without specifying their concrete classes.
+    //    1.4 - Builder Pattern - Seperates the constructions of a complex object from its representation, allowing the same construction to create different representations.
+
+    // 2. Structural Patterns -> 
+    //    2.1 - Adapter Pattern - Allow objects with incompatible interfaces to work together by providing a intermediary that translate requests.
+    //    2.2 - Decorator Pattern - Dynamically adds responsibility to objects by wraping them with one or more decorators.
+    //    2.3 - Facade Pattern - Provides a Unified interfaces in a subsystem, making it easier to use.
+    //    2.4 - Procy Pattern - Provides a surrogate or placeholder for another object to control access to it.
+
+    // 3. Behavioral Patterns ->
+    //    3.1 - Observer Pattern - Defines one-to-many dependency between objects so that when one object changes state, all its dependencies are notified and updated automatically.
+    //    3.2 - Strategy Pattern - Defines a family of algorithms, encapsulates each one, and makes them interchangeable. Strategy lets the algorithm vary independently from clients that use it.
+    //    3.3 - Template Pattern - Defines the skeleton of an algorithm in a method, deferring some steps to subclasses. It allows subclasses to redefine certain steps of an algorithm without changing its structure.
+    //    3.4 - Iterator Pattern - Provides a way to access the elements of an aggregate object sequentially without exposing its underlying representation.
+    //    3.5 - Command Pattern - Encapsulates a request as an object, thereby letting you parameterize clients with different requests, queue or log requests, and support undoable operations.
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // You Will See examples for each design patterns in another tutorials.
+            // Here are only description that how many types of design patterns are available.
+        }
+    }
+}
