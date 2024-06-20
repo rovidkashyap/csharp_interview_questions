a522a520e4bf229bbc3b6ec708a6ece81cee67ef
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-20 19:10:23 +0530

    Question 50

---
+++ OOPS_Concepts/50 - Early Binding and Late Binding/earlyBinding_and lateBinding/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System;
+
+namespace earlyBinding_and_lateBinding
+{
+    // EARLY BINDING
+
+    // Early binding, also known as static binding or compile-time binding, occurs when the method
+    // to be invoked is determined at compile time. In early binding, the compiler has full knowledge
+    // of the method being called, including its signature and type. This allows for type checking
+    // and optimization by the compiler.
+
+    // LATE BINDING
+
+    // Late binding, also known as dynamic binding or runtime binding, occurs when the method to be
+    // invoked is determined at runtime. In late binding, the exact method that will be called is
+    // not known until the code is actually executed. This is often used in scenarios where the
+    // method or object might not be known at compile time.
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Late binding using reflection
+            Assembly assembly = Assembly.Load("mscorlib");
+            Type type = assembly.GetType("System.String");
+            MethodInfo methodInfo = type.GetMethod("Substring", new Type[] { typeof(int), typeof(int) });
+
+            string str = "Hello, World!";
+            object[] parameters = new object[] { 7, 5 };
+            string result = (string)methodInfo.Invoke(str, parameters);
+
+            Console.WriteLine(result); // Output: World
+        }
+    }
+}
---
+++ OOPS_Concepts/50 - Early Binding and Late Binding/earlyBinding_and lateBinding/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("earlyBinding_and lateBinding")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("earlyBinding_and lateBinding")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ed104dc6-9a33-48c0-83e6-cb3c7e620e4f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
