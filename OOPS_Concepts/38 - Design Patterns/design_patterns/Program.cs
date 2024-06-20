81e3973ff4448eee4462d2b4a84f97847aebbab7
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-20 16:13:35 +0530

    Question 38

---
+++ OOPS_Concepts/38 - Design Patterns/design_patterns/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace design_patterns
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Design patterns in C# (and in programming in general) are proven solutions
+            // to common problems that arise during software design and development.
+            // They provide a structured approach to solving recurring design challenges,
+            // promoting code reusability, flexibility, and maintainability.
+        }
+    }
+}
