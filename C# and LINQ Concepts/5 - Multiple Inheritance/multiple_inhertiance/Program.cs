dad53e42be04057b590ab3a2e9ae3891166a88ae
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-28 18:38:48 +0530

    Multiple Inhertiance in C#

---
+++ C# and LINQ Concepts/5 - Multiple Inheritance/multiple_inhertiance/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace multiple_inhertiance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // C# does not support multiple inhertiance for classes, meaning class cannot directly
+            // inherit from more than one base class. The design choice is made to avoid the complexities
+            // and potential issues that arise with multiple inheritance, such as the `diamond problem`.
+        }
+    }
+}
---
+++ C# and LINQ Concepts/5 - Multiple Inheritance/multiple_inhertiance/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("multiple_inhertiance")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("multiple_inhertiance")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b413b4a8-b5d1-47ff-a65f-0ad35aef8218")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
