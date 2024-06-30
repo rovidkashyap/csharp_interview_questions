b94df2dfce43a02969acd50d59b6e2a37ff1ef78
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-07-01 00:02:01 +0530

    Question 16 - ObjectType and DynamicType in C#

---
+++ C# and LINQ Concepts/16 - ObjectType and DynamicType/objecttype_and_dynamictype/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace objecttype_and_dynamictype
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // In C#, both object type variables and dynamic type variables provide flexibility in
+            // handling different types of data, but they differ significantly in how they are
+            // treated by the compiler and at runtime.
+
+            // OBJECT-TYPE VARIABLES (`object`) :
+
+            // 1. Type-checked at compile-time. Required explicit cating to access members.
+            // 2. May involve boxing/unboxing for value types, impacting performance.
+
+            object obj = 42;    // Boxing int to object
+            obj = "hello";      // Assigning a string
+
+            int number = (int)obj;  // Unboxing object to int
+
+            // DYNAMIC_TYPE VARIABLES (`dynamic`) :
+
+            // 1. Type-checked at runtime. Allows member access without casting.
+            // 2. Avoid boxing.unboxing but may incur overhead due to runtime type-resolution.
+
+            dynamic dyn = 42;
+            dyn = "hello";
+
+            dyn += 10;  // OK: treated as int
+            dyn = "hello";
+            dyn += " world";    // OK: treated as string
+        }
+    }
+}
---
+++ C# and LINQ Concepts/16 - ObjectType and DynamicType/objecttype_and_dynamictype/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("objecttype_and_dynamictype")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("objecttype_and_dynamictype")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0118eab8-02f7-4e91-b662-f16d8da0de1d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
