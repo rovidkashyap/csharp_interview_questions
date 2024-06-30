107d4237531a996592e0a20bbf8b3707ee9847de
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-06-30 21:41:31 +0530

    Question 9 - This Command in Static Method

---
+++ C# and LINQ Concepts/9 - This in Static Method/this_in_static_method/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace this_in_static_method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            
+                No, you cannot use the this keyword within a static method in C#. The this keyword 
+                refers to the current instance of the class, and static methods belong to the class 
+                itself, not to any specific instance. Therefore, this is not available in static 
+                methods.
+             
+            */
+        }
+    }
+}
