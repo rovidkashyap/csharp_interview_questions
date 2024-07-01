5a5b0d0732f193fcd0ca522aa2ed24b9b11ba8e4
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-07-01 15:21:50 +0530

    Question 25 - Select<T> vs SelectMany<T> in LINQ

---
+++ C# and LINQ Concepts/25 - Select vs SelectMany/select_vs_selectmany/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace select_vs_selectmany
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+            `Select` - Use `Select` when you want to change each item in a list into something else.
+                     - If you have a list of numbers and you want to turn each number into its square 
+                       (like turning 2 into 4 and 3 into 9), you'd use Select.            
+
+
+            `SelectMany` - Use SelectMany when you have a list of lists and you want to combine them 
+                           all into one big list.
+                         - Imagine you have a list where each item is another list of numbers. If you 
+                           use SelectMany, it will take all those small lists and put all their 
+                           numbers together into one big list.
+            
+             */
+
+            #region SELECt<T>
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+
+            // Selecting each number and transforming it into its square
+            var squares = numbers.Select(num => num * num);
+
+            foreach (var square in squares)
+            {
+                Console.WriteLine(square);
+            }
+
+            #endregion
+
+            #region SELECTMANY<T>
+
+            List<List<int>> nestedList = new List<List<int>>
+            {
+                new List<int>{ 1, 2 },
+                new List<int>{ 3, 4 },
+                new List<int>{ 5, 6 }
+            };
+
+            // Flatterning the nested lists into a single sequence
+            var flattened = nestedList.SelectMany(list => list);
+
+            foreach (var number in flattened)
+            {
+                Console.WriteLine(number);
+            }
+
+            #endregion
+        }
+    }
+}
---
+++ C# and LINQ Concepts/25 - Select vs SelectMany/select_vs_selectmany/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("select_vs_selectmany")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("select_vs_selectmany")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("09c5b6bf-1e84-4115-9d5a-38fe98d08bb0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
