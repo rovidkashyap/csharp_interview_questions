bb22f5572126514faf8c5ac85b41b753094993b9
Rovid Kashyap <rvd.kashyap@gmail.com>
2024-07-04 15:50:17 +0530

    Question 46 - Anonymous Function in LINQ

---
+++ C# and LINQ Concepts/46 - Anonymous Function in LINQ/anonymous_function/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace anonymous_function
+{
+    internal class Program
+    {
+        // In LINQ (Language-Integrated Query), an anonymous function refers to a lambda expression
+        // or a delegate that is defined inline without explicitly declaring a method.
+
+        // Anonymous functions are commonly used in LINQ queries to define predicates, projections,
+        // and transformations directly within the query syntax.
+
+        // Anonymous functions are particularly useful in LINQ for defining predicates
+        // (Where clause), projections (Select clause), sorting (OrderBy clause), and other
+        // operations without needing to define separate named methods.
+
+        static void Main(string[] args)
+        {
+            // Filtering with `Where`
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            var evenNumbers = numbers.Where(x => x % 2 == 0);
+
+            // Projecting with `Select`
+            List<string> words = new List<string> { "apple", "banana", "cherry" };
+            var lengths = words.Select(word => word.Length);
+
+            // Sorting with `OrderBy`
+            List<string> fruits = new List<string> { "apple", "banana", "cherry" };
+            var sortedFruits = fruits.OrderBy(fruit => fruit);
+        }
+    }
+}
